b323f4382c74a27569f0bb12f549ab91ef9935c6
Danielapariona <daniela.parionac@gmail.com>
2018-04-11 19:15:37 -0500

    Step for users

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(".next-step-2").on("click", function () {
+  console.log('next-step-2');
+  $('.step-1').removeClass("active");
+  $('.step-1').addClass("disabled");
+  $('.content-step-1').addClass('hidden');
+  $('.content-step-2').removeClass('hidden');
+});
+
+$('.next-step-3').on("click", function () {
+  console.log('next-step-3');
+  $('.step-2').removeClass("active");
+  $('.step-2').addClass("disabled");
+  $('.content-step-2').addClass('hidden');
+  $('.content-step-4').removeClass('hidden');
+});
+
